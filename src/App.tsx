cc4967aede466c6bbc83e08397eb346952e1db64
DLuz1 <diogolinoluz@hollandandbarrett.com>
2022-12-20 11:35:22 +0000

    Imported the Login Component to the main App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
