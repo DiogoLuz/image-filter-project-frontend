2c2560d9f79f87e510f095dc1c40909854c71aff
DLuz1 <diogolinoluz@hollandandbarrett.com>
2022-12-22 09:45:53 +0000

    Created a Login Field Component

---
+++ components/LoginField.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+function LoginField() {
+  return <div></div>;
+}
+
+export default LoginField;
