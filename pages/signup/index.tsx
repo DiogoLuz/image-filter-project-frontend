d10f085bfb92f395b5a0a5994f1da1f2af3cf9e0
DLuz1 <diogolinoluz@hollandandbarrett.com>
2022-12-23 10:13:02 +0000

    Path props to Navbar

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,7 +1,5 @@
 import { NextPage } from "next";
-import Login from "../../components/LoginField";
 import Navbar from "../../components/Navbar";
-import styles from "../../styles/Login.module.scss";
 import { useRouter } from "next/router";
 
 interface Props {}
@@ -10,10 +8,7 @@ const Index: NextPage<Props> = ({}) => {
   const { asPath } = useRouter();
   return (
     <div className="App">
-      <Navbar path={asPath} />
-      <div className={`${styles.container} flex-align-justify-center`}>
-        <Login />
-      </div>
+      <Navbar path={asPath}></Navbar>
     </div>
   );
 };
